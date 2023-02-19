4aefc27af3849a7edd2d713679c68c5692967bc4
유성원 <swyoo753@naver.com>
2023-02-19 16:21:57 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<MainPage />} /> */}
         <Route path="/" element={<Home />} />
-        <h1>23.01.20</h1>
       </Routes>
     </>
   );
